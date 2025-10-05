4cdbb151e574bd10940feb841daa7b506664d9fe
linamov <linusikvann@gmail.com>
2025-10-05 23:22:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,16 +10,17 @@ const numInput = document.getElementById('numInput');
 const addBtn = document.getElementById('addBtn');
 const resultDiv = document.getElementById('result');
 
-addBtn.addEventListener('click', function() {
+addBtn.onclick = function() {
     const num = Number(numInput.value);
+
     if (!isNaN(num) && num > 0 && Number.isInteger(num)) {
         const currentTotal = sum(num);
         resultDiv.textContent = `Result: ${currentTotal}`;
         numInput.value = '';
         numInput.focus();
     } else {
-        alert('Please enter a valid positive integer');
+        alert("Please enter a valid positive integer");
         numInput.value = '';
         numInput.focus();
     }
-});
+};
